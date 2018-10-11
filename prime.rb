9161d0b15cb03266c294df59edd1d5de0f8ff490
Dana Palmer <roiai4@hotmail.com>
2018-10-11 01:11:09 +0000

    automatically backed up by learn

--- prime.rb
+++ prime.rb
@@ -1,8 +1,8 @@
 def prime?(int)
-  if int <= 0 || int == 1 
+  if int <= 0 || int == 1
     return false
 else
-  (2..max).to_a.all? do |num|
+  (2..-1).to_a.all? do |num|
       int % num != 0
     end
   end
